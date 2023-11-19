ad6323f12958a714eefd7c017fca96b77d9bbfed
Abdulwasim-S <abdulwasimsmech@gmail.com>
2023-11-19 16:28:57 +0530

    welcome

--- src/components/pages/homepage/HomePage.js
+++ src/components/pages/homepage/HomePage.js
@@ -1,13 +1,23 @@
 import React from "react";
 import NavBar from "./NavBar";
 import { Outlet } from "react-router-dom";
-import { Box } from "@chakra-ui/react";
+import { Box, Flex } from "@chakra-ui/react";
 
 const HomePage = () => {
   return (
     <div>
       <Box bg={"gray.300"}>
         <NavBar />
+        <Flex
+          minH={"20vh"}
+          bg={"gray.800"}
+          color={"white"}
+          justifyContent={"center"}
+          alignItems={"center"}
+        >
+          {" "}
+          <i>Habibi come to welcome to ShopZone</i>
+        </Flex>
         <Outlet />
       </Box>
     </div>
